43d0406477143f1664ae970d5158b009d1976fd8
isaquefms <isaquefernando10@gmail.com>
2019-10-23 11:54:43 -0300

    inicio da implementação dos modelos segundo o diagrama de classes

--- settings/base.py
+++ settings/base.py
@@ -29,15 +29,20 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+PROJECT_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.core.apps.CoreConfig',
+    'apps.api.apps.ApiConfig'
 ]
+
+DEPENDENCY_APPS = []
+
+INSTALLED_APPS = PROJECT_APPS + DEPENDENCY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
