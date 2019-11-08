8b5900a944c07b4b8e37ca427a2c4e06d5b961a0
Daniel <gc04346@gmail.com>
2019-11-07 22:25:12 -0300

    modelos prontos

--- apps/core/models.py
+++ apps/core/models.py
@@ -3,17 +3,17 @@ from django.db import models
 
 # Create your models here.
 class Pessoa(models.Model):
-	nome = models.CharField(verbose_name='Nome', max_lenght=100)
-	cpf = models.CharField(verbose_name='CPF', max_lenght=14)
+	nome = models.CharField(verbose_name='Nome', max_length=100)
+	cpf = models.CharField(verbose_name='CPF', max_length=14)
 	email = models.EmailField(verbose_name='Email')
-	user = models.OneToOneField(verbose_name='Usuario', to=User, on_delete=models.CASCADE)
+	# user = models.OneToOneField(verbose_name='Usuario', to=User, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.nome
 
 
 class Cliente(models.Model):
-	telefone = models.CharField(verbose_name='Telefone', max_lenght=14)
+	telefone = models.CharField(verbose_name='Telefone', max_length=14)
 	pessoa = models.OneToOneField(verbose_name='Pessoa', to=Pessoa, on_delete=models.CASCADE)
 
 	def __str__(self):
@@ -23,20 +23,20 @@ class Cliente(models.Model):
 class Hotel(models.Model):
 	nome = models.CharField(verbose_name='Nome', max_length=100)
 	descricao_hotel = models.CharField(verbose_name='Descrição Hotel', max_length=500, null=True, blank=True)
-	foto = models.ImageField(upload_to='/imgs/hoteis/', null=True, blank=True)
+	# foto = models.ImageField(upload_to='/imgs/hoteis/', null=True, blank=True)
 
 	def __str__(self):
 		return self.nome
 
 
 class Quarto(models.Model):
+	hotel = models.ForeignKey(verbose_name='Hotel', to=Hotel, on_delete=models.CASCADE)
+	num_quarto = models.IntegerField(verbose_name='Número de Quartos')
 	num_camas_solteiro = models.IntegerField(verbose_name='Camas de Solteiro')
 	num_camas_casal = models.IntegerField(verbose_name='Camas de Casal')
-	num_quarto = models.IntegerField(verbose_name='Número de Quartos')
-	hotel = models.ForeignKey(verbose_name='Hotel', to=Hotel, on_delete=models.CASCADE)
 
 	def __str__(self):
-		return 'Quarto no hotel ' + self.hotel.name
+		return 'Quarto no hotel ' + self.hotel.nome
 
 
 class RecomendacaoHotel(models.Model):
